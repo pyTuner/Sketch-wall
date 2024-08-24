be69d8d2cbf64c5face1da80f532ddf71a4da636
tejas <pytuner@outlook.com>
2024-08-24 12:11:32 +0530

    added vercel analytics -> to monitor traffic

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './global.css'; //cause 0 padding and margin
 
@@ -7,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
